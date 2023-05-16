d0242d92bda1c6c66cf671265dbe96b86e474271
Jerry Wang <jerry.wang2@hp.com>
2023-05-16 11:06:25 +0800

    refine normlizer performance

--- src/services/api/getCandleStick/getCandleStick.ts
+++ src/services/api/getCandleStick/getCandleStick.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
+import { DailyData, IData, MetaData } from 'pages/CandleStick/types';
 import { APISchema } from './types';
-import { IData } from 'pages/CandleStick/types';
 /**
  * Get response actions
  * @returns {Promise}
@@ -14,17 +14,22 @@ const apiUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&sym
 export default async function getCandleStick(): Promise<IData> {
   let response: AxiosResponse;
   try {
+    const start = performance.now();
+    response = await axios.get(apiUrl);
+    const end = performance.now();
+    console.log(`API query time: ${end - start} ms`);
     response = await axios.get(apiUrl);
   } catch (error) {
     throw new Error('Unable to fetch data');
   }
-  return response.data && normalizer(response.data);
+  const result = response.data && normalizer(response.data);
+  return result;
 }
 
 export function normalizer(rawData: APISchema) {
-  const metaData = rawData['Meta Data'];
-  const timeSeriesData = rawData['Time Series (Daily)'];
-  return {
+  const start = performance.now();
+  const { 'Meta Data': metaData, 'Time Series (Daily)': timeSeriesData } = rawData;
+  const data: { 'Meta Data': MetaData; 'Time Series (Daily)': Record<string, DailyData> } = {
     'Meta Data': {
       Information: metaData['1. Information'],
       Symbol: metaData['2. Symbol'],
@@ -32,18 +37,20 @@ export function normalizer(rawData: APISchema) {
       'Output Size': metaData['4. Output Size'],
       'Time Zone': metaData['5. Time Zone'],
     },
-    'Time Series (Daily)': Object.entries(timeSeriesData).reduce(
-      (acc, [date, data]) => ({
-        ...acc,
-        [date]: {
-          open: data['1. open'],
-          high: data['2. high'],
-          low: data['3. low'],
-          close: data['4. close'],
-          volume: data['5. volume'],
-        },
-      }),
-      {}
-    ),
+    'Time Series (Daily)': {},
   };
+
+  for (const date in timeSeriesData) {
+    data['Time Series (Daily)'][date] = {
+      open: timeSeriesData[date]['1. open'],
+      high: timeSeriesData[date]['2. high'],
+      low: timeSeriesData[date]['3. low'],
+      close: timeSeriesData[date]['4. close'],
+      volume: timeSeriesData[date]['5. volume'],
+    };
+  }
+
+  const end = performance.now();
+  console.log(`normalizer execute time: ${end - start} ms`);
+  return data;
 }
